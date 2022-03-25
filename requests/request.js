efe497d2906e3626bb9c68c1606e0cca2d83fae5
chaos44 <liuchao46642@yahoo.co.jp>
2022-03-25 17:37:07 +0900

    add deep translate url

--- requests/request.js
+++ requests/request.js
@@ -1,5 +1,28 @@
+const base_url = 'https://api-free.deepl.com/v2/translate?auth_key=7aed5fcf-8593-6844-0d5e-ee409fe84cd9%3Afx&source_lang=en&tag_handling=0'
+
 export const requests = {
     RandomWords: {
         url: 'https://random-word-api.herokuapp.com/word?number=10&swear=1',
     },
+    JapaneseWords: {
+        url: `${base_url}&target_lang=ja`, 
+    },
+    GermanWords: {
+        url: `${base_url}&target_lang=de`, 
+    },
+    FrechWords: {
+        url: `${base_url}&target_lang=fr`, 
+    },
+    ItalianWords: {
+        url: `${base_url}&target_lang=it`, 
+    },
+    SpanishWords: {
+        url: `${base_url}&target_lang=es`, 
+    },
+    ChineseWords: {
+        url: `${base_url}&target_lang=zh`, 
+    },
+    RussianWords: {
+        url: `${base_url}&target_lang=ru`, 
+    }
 };
